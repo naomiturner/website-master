47a9cbfc6782f7d45a217d10e950639bca76be03
NaomiTurner <turner.naomi@gmail.com>
2019-02-17 21:09:46 +0000

    Draft

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title> Sandbox </title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+  </head>
+  <body>
+    <h1>I'm Naomi.</h1>
+    <p>Currently Product Manager at HMPPS, Ministry of Justice, researching and building user-centred services for prisoners. Prior to that I worked as a Caseworker for a Member of Parliament, as Head of Design and Manufacturing Policy at a thinktank, and as a Private Secretary at the Ministry of Justice.</p>
+    <p>I have helped to build a tool to support childcare cooperatives, initiated a library of patterns for GDPR, and worked with MPs to use speculative design to explore the future of assisted dying.</p>
+    <p>In my spare time, I co-organise a festival celebrating maintenance and what it can teach us about being better custodians of technology and communities.</p>
+    <p>I'm enthusiastic about making policy tangible through design, data minimisation and dancing.</p>
+    <h2>Contact</h2>
+    <p><a href="http:twitter.com/naomi_turner">I'm on twitter</a><br/>
+    I'm trapped forever in<a href="https://www.linkedin.com/in/naomi-turner-48696434/">Linkedin </a><br/>
+    I occasionally ,<a href="https://medium.com/@Naomi_Turner">blog</a></p>
+
+  </body>
+</html>
